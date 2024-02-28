a086eb0df09e5dd73e5d24f76a8899a7e8a671c6
achiewe <mamukashviliachi706@gmail.com>
2024-02-28 19:48:24 +0400

    fetch the most popular 20 photoes from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 import GlobalStyles from "./GlobalStyles";
 import axios from "axios";
-const airportsApiKey = import.meta.env.VITE_REACT_APP_ACCESS_KEY;
+import { useEffect } from "react";
+const accessKey = import.meta.env.VITE_REACT_APP_ACCESS_KEY;
 
 function App(): JSX.Element {
-  const getImages = async () => {
-    const response = await axios.get(
-      "https://api.unsplash.com/photos/?client_id=YOUR_UNSPLASH_ACCESS_KEY&per_page=20"
-    );
-  };
+  useEffect(() => {
+    const getImages = async () => {
+      const response = await axios.get(
+        `https://api.unsplash.com/photos/?client_id=${accessKey}&order_by=popular&per_page=20`
+      );
+
+      const data = response.data;
+      console.log(data);
+    };
+    getImages();
+  }, []);
 
   return (
     <MainContainer>
