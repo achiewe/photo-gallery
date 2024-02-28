e3ede2546964804fb379fcd94dab1cd82adaf668
achiewe <mamukashviliachi706@gmail.com>
2024-02-28 23:33:58 +0400

    create state and setstate for take the onhange values and add in the array

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,8 @@ import { PhotoesType } from "../type";
 type galleryStore = {
   fetchPhotoes: PhotoesType[];
   inputValue: string;
+  inputValueArray: string[];
+  setInputValueArray: (newArray: string[]) => void;
   setInputValue: (newValue: string) => void;
   setFetchPhotoes: (newphotoes: PhotoesType[]) => void;
 };
@@ -11,6 +13,10 @@ type galleryStore = {
 export const useGalleryStore = create<galleryStore>((set) => ({
   fetchPhotoes: [],
   inputValue: "",
+  inputValueArray: [],
+  setInputValueArray: (newArray: string[]) => {
+    set({ inputValueArray: newArray });
+  },
   setFetchPhotoes: (newphotoes: PhotoesType[]) => {
     set({ fetchPhotoes: newphotoes });
   },
