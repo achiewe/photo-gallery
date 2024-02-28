941a10e1d4a2acae553944e53fa0b698b60ee39a
achiewe <mamukashviliachi706@gmail.com>
2024-02-28 21:48:40 +0400

    add the inputValue state and set state

--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,18 @@ import { PhotoesType } from "../type";
 
 type galleryStore = {
   fetchPhotoes: PhotoesType[];
+  inputValue: string;
+  setInputValue: (newValue: string) => void;
   setFetchPhotoes: (newphotoes: PhotoesType[]) => void;
 };
 
 export const useGalleryStore = create<galleryStore>((set) => ({
   fetchPhotoes: [],
+  inputValue: "",
   setFetchPhotoes: (newphotoes: PhotoesType[]) => {
     set({ fetchPhotoes: newphotoes });
+  },
+  setInputValue: (newValue: string) => {
+    set({ inputValue: newValue });
   },
 }));
