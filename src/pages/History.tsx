d5485de2445cb5f162459bc2b13560216c76501a
achiewe <mamukashviliachi706@gmail.com>
2024-02-28 23:53:28 +0400

    map the inputValueArray in the History component

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,23 @@
+import styled from "styled-components";
+import { useGalleryStore } from "../store";
+
 export default function History() {
-  return <div>History</div>;
+  const inputValueArray = useGalleryStore((state) => state.inputValueArray);
+  return (
+    <MainCont>
+      {inputValueArray.map((inputText) => {
+        return <h1 key={inputText}>{inputText}</h1>;
+      })}
+      <div> </div>
+    </MainCont>
+  );
 }
+
+const MainCont = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2px;
+`;
