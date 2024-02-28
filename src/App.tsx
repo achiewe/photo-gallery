e4241d6d0ff605e4d89a53baaa874e8ebcb019b4
achiewe <mamukashviliachi706@gmail.com>
2024-02-28 19:36:40 +0400

    add the function to fetch the images from the api address and for that create access key

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+import axios from "axios";
+const airportsApiKey = import.meta.env.VITE_REACT_APP_ACCESS_KEY;
+
 function App(): JSX.Element {
-  return <MainContainer> </MainContainer>;
+  const getImages = async () => {
+    const response = await axios.get(
+      "https://api.unsplash.com/photos/?client_id=YOUR_UNSPLASH_ACCESS_KEY&per_page=20"
+    );
+  };
+
+  return (
+    <MainContainer>
+      <GlobalStyles />
+    </MainContainer>
+  );
 }
 
+const MainContainer = styled.div`
+  width: 100%;
+`;
 export default App;
