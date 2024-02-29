ce9298ddcdffc7667f94f506b1c64ed0c7aa1c2c
achiewe <mamukashviliachi706@gmail.com>
2024-02-29 20:52:17 +0400

    create modalWindow for the photoes to show full image

---
+++ src/components/home/ModalWindow.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function ModalWindow() {
+  return <ModalContainer></ModalContainer>;
+}
+
+const ModalContainer = styled.div`
+  width: 300px;
+  height: 100px;
+`;
