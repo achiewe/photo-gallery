eef5fd6354c2d90db9551a3c1e2c09c81a6154ad
achiewe <mamukashviliachi706@gmail.com>
2024-03-01 13:53:44 +0400

    add from the filteredImages how much view likes and donwloads have each photo

--- src/components/home/ModalWindow.tsx
+++ src/components/home/ModalWindow.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { useGalleryStore } from "../../store";
 import { SearchDataType } from "../../../type";
+import likePng from "../../../public/assets/like.png";
+import ViewPng from "../../../public/assets/view.png";
+import downoloadPng from "../../../public/assets/download.png";
 
 export default function ModalWindow() {
   const filteredImages = useGalleryStore((state) => state.filteredImages);
@@ -8,22 +11,42 @@ export default function ModalWindow() {
     <ModalContainer filteredImages={filteredImages}>
       <div className="imageDataContainer">
         <img src={filteredImages[0]?.urls.thumb} />
+        <div className="InfoDiv">
+          <img className="likeViewDownPng" src={likePng} alt="like png" />
+          <h2>likes: {filteredImages[0]?.likes}</h2>
+        </div>
+        <div className="InfoDiv">
+          <img className="likeViewDownPng" src={ViewPng} alt="view png" />
+          <h2>View: {filteredImages[0]?.likes}</h2>
+        </div>
+        <div className="InfoDiv">
+          <img
+            className="likeViewDownPng"
+            src={downoloadPng}
+            alt="download png"
+          />
+          <h2>Downloads: {filteredImages[0]?.likes}</h2>
+        </div>
       </div>
     </ModalContainer>
   );
 }
 
 const ModalContainer = styled.div<{ filteredImages: SearchDataType[] }>`
-  width: 300px;
-  height: 100px;
+  width: 200px;
+  height: 300px;
   background-color: white;
   display: ${(props) => (props.filteredImages.length > 0 ? "flex" : "none")};
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  position: fixed;
-  top: 50%;
+  position: absolute;
+  top: 4%;
   z-index: 1;
+
+  h2 {
+    font-size: 18px;
+  }
 
   .imageDataContainer {
     width: 100%;
@@ -31,5 +54,19 @@ const ModalContainer = styled.div<{ filteredImages: SearchDataType[] }>`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    gap: 15px;
+
+    .InfoDiv {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 5px;
+
+      .likeViewDownPng {
+        width: 20px;
+        height: 20px;
+      }
+    }
   }
 `;
